011f5feaf8f157d2c1040f7fe68ca52577d55e37
William Bennett <wbennett@lanl.gov>
2025-03-10 10:45:16 -0600

    CRC ORDER 8

--- coeff_4d_all_pn.py
+++ coeff_4d_all_pn.py
@@ -15,8 +15,8 @@ tol = 1e-8
 ob.make_all_coefficients_pn(M, 2)
 # ob.save_pn_all()
 coeffs_old = ob.c_Pn_all
-M_list = [6,7]
-order_list = [20, 24, 28]
+M_list = [8]
+order_list = [17]
 it = 0
 while err > tol:
     M = M_list[it]
